f4ebd05cc3fedaaa164d2ee9e02753363d8a96a4
realsanjeev2 <realsanjeev2@gmail.com>
2025-04-04 23:56:57 +0545

    Added GUI using raylib

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -xe
 
-CFLAGS="-Wall -Wextra -ggdb"
+CFLAGS="-Wall -Wextra -ggdb -Wswitch-enum"
+LIBS="-lm -ldl -lpthread"
+RAYLIB_PATH="./raylib/raylib-5.5_linux_amd64"
 
+# Ensure the build directory exists
 mkdir -p build
-gcc $CFLAGS -o ./build/ranomart main.c -lm
 
-# Execute the executable
-./build/ranomart
+cc $CFLAGS -I"$RAYLIB_PATH/include" \
+    main.c -o ./build/ranomart  \
+    -L"$RAYLIB_PATH/lib" -lraylib \
+    $LIBS
+
+# Execute the compiled program
+LD_LIBRARY_PATH=./raylib/raylib-5.5_linux_amd64/lib 
+./build/ranomart gui
